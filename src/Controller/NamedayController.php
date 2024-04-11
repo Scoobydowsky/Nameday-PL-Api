a1024b50f350c6e4290e1e58af7b95322d0ae976
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2024-04-11 12:08:33 +0200

    Create NamedayController.php

---
+++ src/Controller/NamedayController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\SearchService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NamedayController extends AbstractController implements NamedayInterface
+{
+}
